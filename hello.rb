295abff39c9ff3f7c66963a0d832f78a7bbcdc83
Ryuichi UEDA <ueda-ryuichi@aiit.ac.jp>
2014-09-24 09:07:08 +0000

    Envval test2

--- hello.rb
+++ hello.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
 require 'rakuten_web_service'
 
+
+
 get '/' do
-  "hell"
-  ENV["APPID"]
+  RakutenWebService.configuration do |c|
+    c.application_id = ENV["APPID"]
+    c.affiliate_id = ENV["AFID"]
+  end
 end
