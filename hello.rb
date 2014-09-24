03f8d2d7c5475c98433e8f22eeb9fe7b0ec1a627
Ryuichi UEDA <ueda-ryuichi@aiit.ac.jp>
2014-09-24 10:48:19 +0000

    Everything is hell

--- hello.rb
+++ hello.rb
@@ -4,6 +4,7 @@ require 'rakuten_web_service'
 #参考: https://github.com/k2works/sinatra_rakuten_api
 
 get '/' do
+  "hell"
   RakutenWebService.configuration do |c|
     c.application_id = ENV["APPID"]
     c.affiliate_id = ENV["AFID"]
