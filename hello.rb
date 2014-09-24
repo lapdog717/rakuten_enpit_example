dc0c0eaf9507c403eea6e0a8c3046fd0dd85aeae
Ryuichi UEDA <ueda-ryuichi@aiit.ac.jp>
2014-09-24 09:10:01 +0000

    Test Rakuten APP

--- hello.rb
+++ hello.rb
@@ -8,4 +8,21 @@ get '/' do
     c.application_id = ENV["APPID"]
     c.affiliate_id = ENV["AFID"]
   end
+
+  items = RakutenWebService::Ichiba::Item.search(:keyword => 'Ruby') # This returns Enamerable object
+  items.first(10).each do |item|
+    puts "#{item['itemName']}, #{item.price} yen" # You can refer to values as well as Hash. 
+  end
+
+  root = RakutenWebService::Ichiba::Genre.root # root genre
+  # children returns sub genres
+  root.children.each do |child|
+    puts "[#{child.id}] #{child.name}"
+  end
+ 
+  # Use genre id to fetch genre object
+  RakutenWebService::Ichiba::Genre[100316].name # => "水・ソフトドリンク"
+
+  RakutenWebService::Ichiba::Item.ranking(:age => 30, :sex => 0) # 30代男性 のランキングTOP 30
+  RakutenWebService::Ichiba::Genre[100316].ranking # "水・ソフトドリンク" ジャンルのTOP 30
 end
